6dc60ed8d9577221bc3d72b21aed477937acf597
level one <dumale.jeruzael.bscs2019@gmail.com>
2021-11-22 18:43:59 +0800

    exorex

---
+++ README.md
@@ -0,0 +1,1 @@
+# exorex-messaging-app
---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-mkCoR7mvdud4CsmvmZXmtC8vELXkQy4",
+  authDomain: "exorex-messaging-app.firebaseapp.com",
+  projectId: "exorex-messaging-app",
+  storageBucket: "exorex-messaging-app.appspot.com",
+  messagingSenderId: "1061823243057",
+  appId: "1:1061823243057:web:860b9f8bcc1a8bb2144610"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
